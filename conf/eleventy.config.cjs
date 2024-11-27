c535cf05d27e0f7d9718e1f59370cd2e02bab780
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2024-11-27 22:21:01 +0100

    cleaning

--- conf/eleventy.config.cjs
+++ conf/eleventy.config.cjs
@@ -1,4 +1,7 @@
+const eleventyAsciidoc = require("eleventy-plugin-asciidoc");
+
 module.exports = async function(eleventyConfig) {
+        eleventyConfig.addPlugin(eleventyAsciidoc);
         eleventyConfig.addPassthroughCopy('__DATA_DIR__/src/css/style.css');
         eleventyConfig.addWatchTarget('__DATA_DIR__/src/css/style.css');
         eleventyConfig.addPassthroughCopy('__DATA_DIR__/src/assets');
