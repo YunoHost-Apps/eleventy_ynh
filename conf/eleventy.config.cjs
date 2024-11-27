9c702d480c48e0bac65cb15440755dcbffbccdc1
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2024-11-27 15:11:04 +0100

    Update eleventy.config.cjs

--- conf/eleventy.config.cjs
+++ conf/eleventy.config.cjs
@@ -1,10 +1,12 @@
 module.exports = async function(eleventyConfig) {
-        eleventyConfig.addPassthroughCopy("__DATA_DIR__/src/css/style.css");
-        eleventyConfig.addWatchTarget("__DATA_DIR__/src/css/style.css");
+        eleventyConfig.addPassthroughCopy('__DATA_DIR__/src/css/style.css');
+        eleventyConfig.addWatchTarget('__DATA_DIR__/src/css/style.css');
+        eleventyConfig.addPassthroughCopy('__DATA_DIR__/src/assets');
+        
         return {
                 dir: {
-                        input: "__DATA_DIR__/src",
-                        output: "__DATA_DIR__/public",
+                        input: '__DATA_DIR__/src',
+                        output: '__DATA_DIR__/public',
                 },
         };
 };
